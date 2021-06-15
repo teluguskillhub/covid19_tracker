907d2c6c4121eee339bc50b392b14c323eb9103d
Lakshmi Narayana <teluguskillhub@gmail.com>
2021-06-16 00:28:34 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from 'react'
 
-function App() {
+const App = () => {
+  const [data,setData] = useState([]);
+  useEffect(() =>{
+    fetch('https://api.covid19india.org/data.json').then(
+      res => res.json()
+    ).then(jsondata => setData(jsondata.statewise))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <h1>INDIA COVID-19 DASHBOARD</h1>
+        <table className="table">
+          <thead className="thead-dark">
+            <tr>
+              <th>State</th>
+              <th>Confirmed</th>
+              <th>Recovered</th>
+              <th>Deaths</th>
+              <th>Active</th>
+              <th>LastUpdate</th>
+            </tr>
+          </thead>
+
+            <tbody>
+            {data.map((item,index) => {
+                return (
+                  <tr key={index}>
+                  <td>{item.state}</td>
+                  <td>{item.confirmed}</td>
+                  <td>{item.recovered}</td>
+                  <td>{item.deaths}</td>
+                  <td>{item.active}</td>
+                  <td>{item.lastupdatedtime}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+        </table>
+      </center>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
